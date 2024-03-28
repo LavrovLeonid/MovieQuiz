f733f0c335a46bfcbc504a4003a94a864cdd9390
Lavrov Leonid <l.lavrov@kari.com>
2024-03-28 12:41:51 +0500

    feat: Добавлена реализация NetworkClient

---
+++ MovieQuiz/Services/NetworkClient/NetworkClient.swift
@@ -0,0 +1,36 @@
+//
+//  NetworkClient.swift
+//  MovieQuiz
+//
+//  Created by Леонид Лавров on 3/28/24.
+//
+
+import Foundation
+
+struct NetworkClient: NetworkClientProtocol {
+    private enum NetworkError: Error {
+        case codeError
+    }
+    
+    func fetch(url: URL, handler: @escaping (Result<Data, Error>) -> Void) {
+        let request = URLRequest(url: url)
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error {
+                handler(.failure(error))
+                return
+            }
+            
+            if let response = response as? HTTPURLResponse, response.statusCode < 200 || response.statusCode >= 300 {
+                handler(.failure(NetworkError.codeError))
+                return
+            }
+            
+            if let data {
+                handler(.success(data))
+            }
+        }
+        
+        task.resume()
+    }
+}
