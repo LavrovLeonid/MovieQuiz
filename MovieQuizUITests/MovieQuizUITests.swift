b3a2495e7cdd0ec1500143c73d26d10c17bd7fe9
Lavrov Leonid <l.lavrov@kari.com>
2024-04-12 22:20:36 +0500

    feat: Добавлен таргет MovieQuizUITests

---
+++ MovieQuizUITests/MovieQuizUITests.swift
@@ -0,0 +1,33 @@
+//
+//  MovieQuizUITests.swift
+//  MovieQuizUITests
+//
+//  Created by Леонид Лавров on 4/12/24.
+//
+
+import XCTest
+
+final class MovieQuizUITests: XCTestCase {
+    
+    var app: XCUIApplication!
+
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        
+        app = XCUIApplication()
+        app.launch()
+        
+        continueAfterFailure = false
+    }
+
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+        
+        app.terminate()
+        app = nil
+    }
+    
+    func testScreenCast() throws {
+        
+    }
+}
